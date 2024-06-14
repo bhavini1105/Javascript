1ae1c96c86dd561fbeed072c20e01660980ecd39
bhavini1105 <bhavinipatel7532@gmail.com>
2024-06-14 09:48:47 +0530

    JavaScript

---
+++ java-beg/script.js
@@ -0,0 +1,9 @@
+//  let js='amazing';
+
+//  if(js === 'amazing') alert(`Javascript is amazing language`);
+
+ let country= "India";
+ let continent = "Delhi";
+ let population = "1.2 billlions"
+
+ console.log(country , continent , population)
---
+++ java-op/script.js
@@ -0,0 +1,42 @@
+
+// first program
+
+var a,b,c;
+
+a=10;
+b=20;
+
+console.log("Value of A",a,"Value of B",b);
+
+c=b;
+b=a;
+a=c;
+
+console.log("After Swepping VAlues");
+console.log("Value of A", a,"Value of B",b);
+
+// second program
+
+let x=2;
+console.log(x);
+
+let result=x**2;
+
+console.log("Squre of Number",result);
+
+// third program
+
+let total=180;
+let anglea=90;
+let angleb=30;
+  
+c = anglea + angleb;
+
+// console.log(c);
+
+let anglec= 180 - c;
+
+console.log("Thrid Angle of triangle",anglec);
+
+
+// document.getElementById("demo").innerHTML=10*5;
