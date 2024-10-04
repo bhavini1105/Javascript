c8ec7bc121aa986bf5e0e6ecc7dc5832107a4cb7
bhavini1105 <bhavinipatel7532@gmail.com>
2024-10-04 11:14:09 +0530

    javscript programs

---
+++ java-array/script.js
@@ -0,0 +1,64 @@
+// console.log();
+
+// let number = [10,20,30,40,50];
+// let double = number.map(a => a*2);
+// console.log(double);
+
+
+// let arr = [1,2,3,4,5];
+// let arr1 = arr.filter(b => b % 2 == 0);
+// console.log(arr1);
+
+/* 
+map: direct 
+*/
+
+// function m(a) {
+//     return a*2;
+// }
+// function n(a) {
+//     return a%2;
+// }
+
+// let num = [1,2,3,4,5,6];
+// console.log(num.length);
+// a=num.length;
+// a/=2;
+// console.log(num[a]); 
+
+
+// let doub = num.map(m);
+// let doub1 = num.filter(n);
+// console.log(doub);
+// console.log(doub1);
+
+
+// let num1 = [10,20,30,40,50,60];
+// num1.forEach(c => console.log(m(c)));
+
+/*  give the value of index of array if element is not in the arrya returns -1  */
+
+// let fruits = ['banana', 'apple','orange','mango','kiwi'];
+// console.log(fruits.indexOf('apple'));
+
+// function even(num) {
+//     return (num % 2 == 0);
+// }
+
+/* gives the booleans true and false /  suppose all elements is divisible by given number then gives rue but if any of number is not divisible by 
+then returns false */
+
+// let numbers = [10,20,31,40,50,60];
+// console.log(numbers.every(num => num % 2 == 0)); // output = false
+
+/* */
+
+// let num = [10,20,30,40,50,60];
+// console.log(num.some(num1 => num1 % 2 == 0));
+
+/* returns boolean values - if element is present => returns true , not peresent => returns false */
+
+// let fruit = ['apple','banana','mango','kiwi'];
+// console.log(fruit.includes('chikoo'));
+
+let fruits = ['banana', 'apple','orange','mango','kiwi'];
