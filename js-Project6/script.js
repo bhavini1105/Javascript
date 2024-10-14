8efcaddc72a7f6c6a6f617cdb80a9e47419bc6a8
bhavini1105 <bhavinipatel7532@gmail.com>
2024-10-14 11:03:59 +0530

    Slider Project

---
+++ js-Project6/script.js
@@ -0,0 +1,12 @@
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+next.addEventListener('click',function () {
+    let item = document.querySelectorAll('.item');
+    document.querySelector('.slider').appendChild(item[0]);
+})
+
+prev.addEventListener('click',function () {
+    let item = document.querySelectorAll('.item');
+    document.querySelector('.slider').prepend(item[item.length - 1]);
+})
